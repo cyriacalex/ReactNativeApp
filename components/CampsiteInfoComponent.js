5721903eb055e2cda1890870c02ed74b3ca16c68
Alex Cyriac <alexccyriac@gmail.com>
2021-06-02 22:21:18 -0500

    Added animations

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postComment, postFavorite } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -24,11 +25,11 @@ function RenderComments({ comments }) {
         return (
             <View style={{ margin: 10 }}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
-                <Rating 
-                readonly 
-                startingValue={item.rating} 
-                imageSize={10} 
-                style={{ fontSize: 12, alignItems:'flex-start', paddingVertical:'5%' }} 
+                <Rating
+                    readonly
+                    startingValue={item.rating}
+                    imageSize={10}
+                    style={{ fontSize: 12, alignItems: 'flex-start', paddingVertical: '5%' }}
                 />
                 <Text style={{ fontSize: 12 }}>{`-- ${item.author}, ${item.date}`}</Text>
             </View>
@@ -36,13 +37,15 @@ function RenderComments({ comments }) {
     };
 
     return (
-        <Card title='Comments'>
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </Card>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
+            <Card title='Comments'>
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Card>
+        </Animatable.View>
     );
 }
 
@@ -51,33 +54,35 @@ function RenderCampsite(props) {
 
     if (campsite) {
         return (
-            <Card
-                featuredTitle={campsite.name}
-                image={{ uri: baseUrl + campsite.image }}>
-                <Text style={{ margin: 10 }}>
-                    {campsite.description}
-                </Text>
-                <View style={styles.cardRow}>
-                    <Icon
-                        name={props.favorite ? 'heart' : 'heart-o'}
-                        type='font-awesome'
-                        color='#f50'
-                        raised
-                        reverse
-                        onPress={() => props.favorite ?
-                            console.log('Already set as a favorite') : props.markFavorite()}
-                    />
-                    <Icon
-                        name='pencil'
-                        type='font-awesome'
-                        color='#5637DD'
-                        raised
-                        reverse
-                        onPress={() => props.onShowModal()}
-                    >
-                    </Icon>
-                </View>
-            </Card>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card
+                    featuredTitle={campsite.name}
+                    image={{ uri: baseUrl + campsite.image }}>
+                    <Text style={{ margin: 10 }}>
+                        {campsite.description}
+                    </Text>
+                    <View style={styles.cardRow}>
+                        <Icon
+                            name={props.favorite ? 'heart' : 'heart-o'}
+                            type='font-awesome'
+                            color='#f50'
+                            raised
+                            reverse
+                            onPress={() => props.favorite ?
+                                console.log('Already set as a favorite') : props.markFavorite()}
+                        />
+                        <Icon
+                            name='pencil'
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => props.onShowModal()}
+                        >
+                        </Icon>
+                    </View>
+                </Card>
+            </Animatable.View >
         )
     }
     return <View />;
@@ -165,7 +170,7 @@ class CampsiteInfo extends Component {
                             onChangeText={textValue => this.setState({ text: textValue })}
                             value={this.state.text}
                         />
-                        <View  style={{ margin: 10 }}>
+                        <View style={{ margin: 10 }}>
                             <Button
                                 title='Submit'
                                 color='#5637DD'
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
 
@@ -8,19 +9,21 @@ class Contact extends Component {
         title: 'Contact Us'
     }
 
-    render(){
+    render() {
         return (
             <ScrollView>
-                <Card 
-                wrapperStyle={{margin:20}}
-                title='Contact Information'
-                >
-                    <Text>1 Nucamp Way</Text>
-                    <Text>Seattle, WA 98001</Text>
-                    <Text>U.S.A.</Text>
-                    <Text style={{marginTop:10}}>Phone: 1-206-555-1234</Text>
-                    <Text>Email: campsites@nucamp.co</Text>
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card
+                        wrapperStyle={{ margin: 20 }}
+                        title='Contact Information'
+                    >
+                        <Text>1 Nucamp Way</Text>
+                        <Text>Seattle, WA 98001</Text>
+                        <Text>U.S.A.</Text>
+                        <Text style={{ marginTop: 10 }}>Phone: 1-206-555-1234</Text>
+                        <Text>Email: campsites@nucamp.co</Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }

7d96d560c91a6554a97a178d0b2090e25b924480
Alex Cyriac <alexccyriac@gmail.com>
2021-06-05 12:19:13 -0500

    Workshop Assignment

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -323,22 +323,26 @@ class Main extends Component {
         this.props.fetchPromotions();
         this.props.fetchPartners();
 
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
-
-        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-            this.handleConnectivityChange(connectionInfo);
-        });
+        this.showNetInfo();
     }
 
-    componentWillUnmount() {
-        this.unsubscribeNetInfo();
+    // componentWillUnmount() {
+    //     this.unsubscribeNetInfo();
+    // }
+
+    showNetInfo = async () => {
+        const fetchedData = await NetInfo.fetch();
+
+        (Platform.OS === 'ios')
+            ? Alert.alert('Initial Network Connectivity Type:', fetchedData.type)
+            : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                fetchedData.type, ToastAndroid.LONG)
+
+        // this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+        //     this.handleConnectivityChange(connectionInfo);
+        // });
     }
-    
+
     handleConnectivityChange = connectionInfo => {
         let connectionMsg = 'You are now connected to an active network.';
         switch (connectionInfo.type) {
